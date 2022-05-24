f05e602c12f70f65808fb960477dd913d3f80e0d
Nassim Kissi <nassimkissi@icloud.com>
2022-05-24 15:28:06 +0200

    add a first planet & change background color

--- js/main.js
+++ js/main.js
@@ -17,21 +17,36 @@ function createCamera(scene){
 
 function createLight(scene){
     const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene)
+    light.intensity = 0.5
+    light.groundColor = new BABYLON.Color3(0, 0, 1)
 }
 
 function createSun(scene){
     const sun = BABYLON.MeshBuilder.CreateSphere('sun', { segments: 16, diameter: 4}, scene)
 }
 
+function createPlanet(scene){
+    const planetMaterial = new BABYLON.StandardMaterial('planetMaterial', scene)
+    planetMaterial.diffuseTexture = new BABYLON.Texture('assets/images/sand.png', scene)
+    planetMaterial.specularColor = BABYLON.Color3.Black()
+
+    const planet = BABYLON.MeshBuilder.CreateSphere('planet', { segments: 16, diameter: 1}, scene)
+    planet.position.x = 4
+    planet.material = planetMaterial
+}
+
 function createScene(){
     // create our scene
     const scene = new BABYLON.Scene(engine)
+    scene.clearColor = BABYLON.Color3.Black()
 
     createCamera()
 
     createLight(scene)
 
     createSun(scene)
+
+    createPlanet(scene)
 
     return scene
 }
